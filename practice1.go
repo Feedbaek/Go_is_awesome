46363b0e41357342e70c2cade4a9f4672a9387f9
Feedbaek <nicek789@naver.com>
2024-04-21 01:47:56 +0900

    practice1: 슬라이스 append() 동작 테스트

--- practice1.go
+++ practice1.go
@@ -12,25 +12,25 @@ func main() {
 	// 슬라이스에 append()를 사용할 때, append의 반환 값으로 기존 슬라이스를 덮어써야 한다.
 	// 그렇지 않으면 append() 사용 시, cap이 부족한 경우 새로운 슬라이스를 생성하게 되고, 기존 슬라이스와는 다른 주소를 가리키게 된다.
 
-	slices := []int{1, 2, 3, 4, 5}
+	var slice []int = []int{1, 2, 3, 4, 5}
 	// cap이 부족하기에 새로운 슬라이스를 생성하고, 새로운 슬라이스에 6을 추가한다.
-	newSlices := append(slices, 6)
-	fmt.Println("first append: ", slices, newSlices)
-	// slices의 첫 번째 요소를 변경하면, newSlices에는 영향을 미치지 않는다.
-	slices[0] = 100
-	fmt.Println("slices changed: ", slices, newSlices)
+	newSlice := append(slice, 6)
+	fmt.Println("first append: ", slice, newSlice)
+	// slice의 첫 번째 요소를 변경하면, newSlice에는 영향을 미치지 않는다.
+	slice[0] = 100
+	fmt.Println("slice changed: ", slice, newSlice)
 
-	// newSlices에 7을 추가한다. 이때, cap이 부족하지 않기에 새로운 슬라이스를 생성하지 않는다.
-	new2Slices := append(newSlices, 7)
-	fmt.Println("second append: ", slices, newSlices, new2Slices)
-	// 같은 주소를 가르키기 때문에 newSlices의 첫 번째 요소를 변경하면, new2Slices에도 영향을 미친다.
-	newSlices[0] = 200
-	fmt.Println("newSlices changed: ", slices, newSlices, new2Slices)
+	// newSlice에 7을 추가한다. 이때, cap이 부족하지 않기에 새로운 슬라이스를 생성하지 않는다.
+	new2Slice := append(newSlice, 7)
+	fmt.Println("second append: ", slice, newSlice, new2Slice)
+	// 같은 주소를 가르키기 때문에 newSlice의 첫 번째 요소를 변경하면, new2Slice에도 영향을 미친다.
+	newSlice[0] = 200
+	fmt.Println("newSlice changed: ", slice, newSlice, new2Slice)
 
-	// 주소를 출력해보면, newSlices와 new2Slices가 포인터로 같은 배열 주소를 가리키는 것을 확인할 수 있다.
-	fmt.Printf("[array address] slices : %p, newSlices: %p, new2Slices: %p\n", slices, newSlices, new2Slices)
+	// 주소를 출력해보면, newSlice와 new2Slice가 포인터로 같은 배열 주소를 가리키는 것을 확인할 수 있다.
+	fmt.Printf("[array address] slice : %p, newSlice: %p, new2Slice: %p\n", slice, newSlice, new2Slice)
 	// 하지만 실제 슬라이스의 주소는 다르다. 다른 객체이기 때문이다.
-	fmt.Printf("[slice address] slices: %p, newSlices: %p, new2Slices: %p\n", &slices, &newSlices, &new2Slices)
-	// 길이와 용량을 출력해보면, newSlices와 new2Slices의 길이가 다르고 용량은 같다는 것을 확인할 수 있다.
-	fmt.Println(len(slices), cap(slices), len(newSlices), cap(newSlices), len(new2Slices), cap(new2Slices))
+	fmt.Printf("[slice address] slice: %p, newSlice: %p, new2Slice: %p\n", &slice, &newSlice, &new2Slice)
+	// 길이와 용량을 출력해보면, newSlice와 new2Slice의 길이가 다르고 용량은 같다는 것을 확인할 수 있다.
+	fmt.Println(len(slice), cap(slice), len(newSlice), cap(newSlice), len(new2Slice), cap(new2Slice))
 }
